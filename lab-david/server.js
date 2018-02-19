a5224c3746287fa752bfbd45296460933ec7a355
DKosmos <davidakosmos@gmail.com>
2018-02-19 13:54:06 -0800

    baseline server active

--- lab-david/server.js
+++ lab-david/server.js
@@ -8,10 +8,38 @@ const server = net.createServer();
 const ee = new EE();
 const clientPool = [];
 
-ee.on('default', function(client){
-  client.socket.write('not an accepted command - type "@help" for a list of supported commands\n');
+ee.on('default', function(client, string){
+  client.socket.write(`${string} is not an accepted command - type "@help" for a list of supported commands\n`);
 });
 
+ee.on('@help', function(client){
+  client.socket.write(`@all <message> - sends a message to all active users\n
+  @dm <nickname> <message> - send a message to the user with the associated nickname\n
+  @list - printout all active users\n
+  @nickname <newnickname> - change your nickname to the newnickname\n`);
+});
+
+ee.on('@all', function(client, string){
+  clientPool.forEach(c => {
+    c.socket.write(`${client.nickname}: ${string}`);
+  });
+});
+
+ee.on(`@dm`, function(client, string){
+  var namefound = false;
+  var nickname = string.split(' ').shift().trim();
+  var msg = string.split(' ').splice(1).join(' ').trim();
+  clientPool.forEach(c => {
+    if(c.nickname === nickname){
+      c.socket.wriote(`${client.nickname}: ${msg}`);
+      namefound = true;
+      return;
+    }
+  });
+  if(!namefound){
+    client.socket.write('that nickname was not found');
+  }
+});
 
 
 server.on('connection', function(socket){
@@ -26,8 +54,8 @@ server.on('connection', function(socket){
       return;
     }
 
-    ee.emit('default', client, data.toString())
-  })
-})
+    ee.emit('default', client, data.toString());
+  });
+});
 
 server.listen(PORT, () => console.log(`listening on ${PORT}`));
