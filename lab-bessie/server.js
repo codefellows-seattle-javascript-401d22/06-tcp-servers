f4cec743cd1679db0aa83efb11529b04d1ed8cfb
Bessie Arino <brarino@gmail.com>
2018-02-19 13:21:24 -0800

    got @help to work

--- lab-bessie/server.js
+++ lab-bessie/server.js
@@ -20,12 +20,12 @@ ee.on('@all', function(client, string) {
 ee.on('@nickname' , function(client, string) {
   let nickname = string.split(' ').shift().trim();
   client.nickname = nickname;
-  client.socket.write(`user nickname has been changed to ${nickname}\n)`);
+  client.socket.write(`user nickname has been changed to ${nickname}\n`);
 });
 
 // allows you to direct message a user
 ee.on('@dm', function(client, string) {
-  var nickname = string.split(' ').shift().trin();
+  var nickname = string.split(' ').shift().trim();
   var message = string.split(' ').splice(1).join(' ').trim();
   pool.forEach(c => {
     if(c.nickname === nickname) {
@@ -34,9 +34,18 @@ ee.on('@dm', function(client, string) {
   });
 });
 
+ee.on('@quit', function(client, string) {
+  pool.forEach(c => c.socket.write(`${client.nickname} has left the group\n`));
+  console.log('Connection closed');
+  client.socket.destroy();
+  pool.pop(this.client);
+  pool.forEach(c => c.socket.write(`There are ${pool.length} users remaining!\n`));
+});
+
 // gives you possible commands when you type @help
-ee.on('@help', function(client, string) {
-  client.socket.write('Commands that you can use: \n @nickname - allows you to change your nickname \n @help - shows you possible commands\n');
+ee.on('@help', function(client) {
+  console.log('client:', client);
+  client.socket.write('what happens when you do @help\n');
 });
 
 ee.on('default', function(client, string) {
@@ -48,10 +57,16 @@ server.on('connection', function(socket) {
   pool.push(client);
 
   socket.on('data', function(data) {
-    const command = data.toString().split(' ').shift();
+    const command = data.toString().split(' ').shift().trim();
+    console.log('command:', JSON.stringify(command));
+    // if(command === '@help') {
+    //   ee.emit(command, client);
+    //   return;
+    // }
     if(command.startsWith('@')) {
       ee.emit(command, client, data.toString().split(' ').splice(1).join(' '));
       console.log('my command after the at:', data.toString().split(' ').splice(1).join(' '));
+      console.log('stringified', JSON.stringify(data.toString().split(' ').splice(1).join(' ')));
       return;
     }
     ee.emit('default', client, data.toString());
