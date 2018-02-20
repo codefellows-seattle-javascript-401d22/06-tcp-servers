57a9b045cc144e1981e3b557f8d5827bad616956
nicoleweese <nweese@ymail.com>
2018-02-20 09:09:20 -0800

    lintr

--- lab-nicole/model/client.js
+++ lab-nicole/model/client.js
@@ -4,6 +4,6 @@ const uuidv4 = require('uuid/v4');
 
 module.exports = function(socket) {
   this.socket = socket;
-  this.nickname = `user_${Math.floor(Math.random() * 10000)}`
+  this.nickname = `user_${Math.floor(Math.random() * 10000)}`;
   this.id = uuidv4();
 };
