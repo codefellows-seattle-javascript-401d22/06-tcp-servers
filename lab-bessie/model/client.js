89f43350650a9080ef2b2520ab88361d0156f6e0
Bessie Arino <brarino@gmail.com>
2018-02-20 10:03:27 -0800

    fixed linter issue

--- lab-bessie/model/client.js
+++ lab-bessie/model/client.js
@@ -2,7 +2,7 @@
 
 const uuidv4 = require('uuid/v4');
 
-const Client = module.exports = function(socket) {
+module.exports = function(socket) {
   this.socket = socket;
   this.nickname = `user_${Math.random()}`;
   this.id = uuidv4();
