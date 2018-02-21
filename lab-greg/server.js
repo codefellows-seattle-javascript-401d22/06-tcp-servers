ef71a63b190288f3a62a72fab2f801d1c58693f5
Your Name <nords3x4@gmail.com>
2018-02-20 20:24:13 -0800

    fixed bad stuff

--- lab-greg/server.js
+++ lab-greg/server.js
@@ -9,11 +9,17 @@ const ee = new EE();
 
 const userPool = [];
 
+ee.on('default', function(client) {
+  client.socket.write('not a command, please use an @ sign \n');
+});
+
 ee.on('@group', function(client, string) {
   userPool.forEach( c => {
-    c.socket.write(`${client.nickname}: ${string}`);
+    c.socket.write(`${client.handle}: ${string}`);
   });
 });
+
+
 
 ee.on('@handle', function(client, string) {
   let handle = string.split(' ').shift().trim();
@@ -21,16 +27,40 @@ ee.on('@handle', function(client, string) {
   client.socket.write(`Your new handle is ${handle}\n`);
 });
 
+ee.on('@im', function(client, string) {
+  var handle = string.split(' ').shift().trim();
+  var message = string.split(' ').splice(1).join(' ').trim();
+  userPool.forEach( c => {
+    if(c.handle === handle) {
+      c.socket.write(`${client.handle}: ${message}`);
+    }
+  });
+});
+
+ee.on('@list', function(client) {
+  userPool.forEach(c => {
+    client.socket.write(c.handle);
+  });
+});
+
+ee.on('@quit', function(server){
+  server.end();
+} );
+
 server.on('connection', function(socket) {
   var client = new Client(socket);
   userPool.push(client);
+  
 
   socket.on('data', function(data){
     const command = data.toString().split(' ').shift().trim();
+    console.log(command);
     if (command.startsWith('@')) {
-      ee.emit(command, client, data.toString().split(' ').splice(1).join(' '));
+      ee.emit(command, client, data.toString().split(' ').slice(1).join(' '));
       return;
     }
+    ee.emit('default', client, data.toString());
+  
   });
 });
 
