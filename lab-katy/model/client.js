a2ea56ea2b52159157ac37aab9814fb88959e2cf
Katy Robinson <katyorob@gmail.com>
2018-02-21 18:38:14 -0800

    resubmit

--- lab-katy/model/client.js
+++ lab-katy/model/client.js
@@ -3,8 +3,8 @@
 const uuidv4 = require('uuid/v4');
 
 const Client = module.exports = function(socket) {
-    this.socket = socket;
-    this.nickname = `user_${Math.random()}`;
-    this.id = uuidv4();
+  this.socket = socket;
+  this.nickname = `user_${Math.random()}`;
+  this.id = uuidv4();
 }
 
--- lab-katy/server.js
+++ lab-katy/server.js
@@ -10,25 +10,24 @@ const ee = new EE();
 const pool = [];
 
 ee.on('@all', function(client, string) {
-    pool.forEach( c => {
-        c.socket.write(`${client.nickname}: ${string}`);
-    });
+  pool.forEach( c => {
+    c.socket.write(`${client.nickname}: ${string}`);
+  });
 });
 
-//not sure how this one works?? how it know
-ee.on('default', function(client, string){
-    client.socket.write('not a command - please use an @ symbol \n');
+ee.on('default', function(client){
+  client.socket.write('not a command - please use an @ symbol \n');
 });
 
 ee.on('@dm', function(client, string){
-    var nickname = string.split(' ').shift().trim();
-    var message = string.split(' ').splice(1).join(' ').trim();
+  var nickname = string.split(' ').shift().trim();
+  var message = string.split(' ').splice(1).join(' ').trim();
 
-    pool.forEach( c => {
-      if(c.nickname === nickname){
-          c.socket.write(`${client.nickname}: ${message}`);
-      }
-    });
+  pool.forEach( c => {
+    if(c.nickname === nickname){
+      c.socket.write(`${client.nickname}: ${message}`);
+    }
+  });
 });
 
 ee.on('@nickname', function(client, string) {
@@ -40,33 +39,37 @@ ee.on('@nickname', function(client, string) {
 server.on('connection', function(socket){
   var client = new Client(socket);
   pool.push(client);
-//   console.log('MY POOL: ', pool)
-  client.socket.write('Welcome to TCP chat!\n To create a new nickname, type the command @nickname + <your new nickname>\n To view a list of current users, type the command @list\n To quit this chat, type the command @quit \nHappy Chatting!')
+ 
+  client.socket.write('Welcome to TCP chat!\n To create a new nickname, type the command @nickname + <your new nickname>\n To view a list of current users, type the command @list\n To quit this chat, type the command @quit \nHappy Chatting!');
   socket.on('data', function(data){
-      const command = data.toString().split(' ').shift().trim();
+    const command = data.toString().split(' ').shift().trim();
 
-      if(command.startsWith('@')){
-        ee.emit(command, client, data.toString().split(' ').splice(1).join(' '));
-        // console.log('my command after @:', data.toString().split(' ').splice(1).join(' '));
-        return;
-      }
-      ee.emit('default', client, data.toString());
+    if(command.startsWith('@')){
+      ee.emit(command, client, data.toString().split(' ').splice(1).join(' '));
+      return;
+    }
+    ee.emit('default', client, data.toString());
   });
+  socket.on('close', function(){
+    const index = pool.indexOf(client);
+    pool.splice(index);
+  });
+  socket.on('error', console.log);
 }); 
 
 ee.on('@list', function(client) {
-    let list = [];
-    pool.forEach( c => {
-      list.push(c.nickname);
-    })
-    client.socket.write(`List of current users: ${list}`);
+  let list = [];
+  pool.forEach( c => {
+    list.push(c.nickname);
+  });
+  client.socket.write(`List of current users: ${list}`);
 });
 
 ee.on('@quit', function(client){
-    client.socket.write('You are now disconnected\n');
-    client.socket.end();
-})
+  client.socket.write('You are now disconnected\n');
+  client.socket.end();
+});
 
 server.listen(PORT, () => {
-    console.log(`listening on ${PORT}`);
+  console.log(`listening on ${PORT}`);
 });
